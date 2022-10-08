32ebbcce24a2a4c41de1bbbd3a3721957afb914c
Daniel Carr <92411898+DCPenguin73@users.noreply.github.com>
2022-10-07 19:44:00 -0600

    all stuff

--- chamber/js/base.js
+++ chamber/js/base.js
@@ -1,5 +1,16 @@
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const today = new Date();
+let day = days[today.getDay()] + ", " + (today.getDate()) + " " + (months[today.getMonth()]) + " " + (today.getFullYear());
+document.querySelector("header div span").textContent = day;
+document.querySelector("footer div span").textContent = today.getFullYear();
+document.getElementById("lastupdatedate").textContent = document.lastModified;
 
 
 function toggleMenu(){
-    document.getElementById("navbar").classList.toggle("open")
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
 }
+
+const x = document.getElementById('hambugerBtn');
+x.onclick = toggleMenu;
