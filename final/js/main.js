5482b37fac2c76c05164928b2dae7b0e4e484904
Daniel Carr <92411898+DCPenguin73@users.noreply.github.com>
2022-12-12 12:30:15 -0700

    final

---
+++ final/js/main.js
@@ -0,0 +1,21 @@
+function toggleMenu(){
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById('hambugerBtn');
+x.onclick = toggleMenu; 
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const today = new Date();
+let day = days[today.getDay()] + ", " + (today.getDate()) + " " + (months[today.getMonth()]) + " " + (today.getFullYear());
+document.querySelector("header div span").textContent = day;
+document.querySelector("footer div span").textContent = today.getFullYear();
+document.getElementById("lastupdatedate").textContent = document.lastModified;
+
+if ((today.getDay() == 1)||(today.getDay == 2)){
+// if (today.getDay() == 2){
+    const pthing = document.querySelector("header p");
+    pthing.style.display = 'block';
+}
